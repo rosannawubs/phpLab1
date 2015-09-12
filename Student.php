7b34432185ff29cc924b5b0a83040f4c42d57313
user <user@user-PC.telus>
2015-09-11 20:41:24 -0700

    pretty-ing it 3

--- Student.php
+++ Student.php
@@ -8,10 +8,14 @@
 
 /**
  * Description of Student
+ * Students can have names, emails and grades
+ * The average of the grades will be displayed
  *
- * @author user
+ * @author Rosanna
  */
 class Student {
+    
+    // sets up a new student
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
@@ -19,14 +23,18 @@ class Student {
         $this->grades = array();
     }
     
+    // sets up the email for the student
     function add_email($which,$address) {
         $this->emails[$which] = $address;
     }
 
+    // adds another grade
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
     
+    
+    // calculates the average of the grades input
     function average() {
         $total = 0;
         foreach ($this->grades as $value)
@@ -34,6 +42,8 @@ class Student {
         return $total / count($this->grades);
     }
     
+    
+    // makes the student object data into nice paragraph
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' ('.$this->average().")\n";
